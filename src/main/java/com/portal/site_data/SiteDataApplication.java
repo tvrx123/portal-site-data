8d1b0404b02ba59e07c9bf83520096a1bb6ca168
tvrx <tvrtkopetrovic33@gmail.com>
2024-08-29 15:59:53 +0200

    Initial commit

---
+++ src/main/java/com/portal/site_data/SiteDataApplication.java
@@ -0,0 +1,13 @@
+package com.portal.site_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portal/site_data/SiteDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.site_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
